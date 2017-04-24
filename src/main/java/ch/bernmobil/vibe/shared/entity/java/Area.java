b77d4a93769a3604b406c23c52ee3b591178eb20
Oliviero Chiodo <oliviero.chiodo@hsr.ch>
2017-04-24 16:57:07 +0200

    Initial commit

---
+++ src/main/java/ch/bernmobil/vibe/shared/entity/java/Area.java
@@ -0,0 +1,27 @@
+package ch.bernmobil.vibe.shared.entity.java;
+
+public class Area {
+    private long id;
+    private String name;
+
+    public Area(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/ch/bernmobil/vibe/shared/entity/java/Journey.java
@@ -0,0 +1,37 @@
+package ch.bernmobil.vibe.shared.entity.java;
+
+public class Journey {
+    private long id;
+    private String headsign;
+    private long route;
+
+    public Journey(long id, String headsign, long route) {
+        this.id = id;
+        this.headsign = headsign;
+        this.route = route;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getHeadsign() {
+        return headsign;
+    }
+
+    public void setHeadsign(String headsign) {
+        this.headsign = headsign;
+    }
+
+    public long getRoute() {
+        return route;
+    }
+
+    public void setRoute(long route) {
+        this.route = route;
+    }
+}
