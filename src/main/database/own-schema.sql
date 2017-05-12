6c31d5195043adc56a06fec76c86543598d80420
matteopatisso <matteo.patisso@gmail.com>
2017-05-12 16:24:18 +0200

    add update history to own-schema.sql

--- src/main/database/own-schema.sql
+++ src/main/database/own-schema.sql
@@ -7,6 +7,7 @@ DROP TABLE IF EXISTS calendar_exception CASCADE;
 DROP TABLE IF EXISTS schedule CASCADE;
 DROP TABLE IF EXISTS schedule_update CASCADE;
 DROP TABLE IF EXISTS journey_disruption CASCADE;
+DROP TABLE IF EXISTS update_history CASCADE;
 
 CREATE TABLE area (
   id UUID PRIMARY KEY,
@@ -29,6 +30,7 @@ CREATE TABLE journey (
   id UUID PRIMARY KEY,
   headsign TEXT,
   route UUID,
+  terminal_station UUID,
   update TIMESTAMP
 );
 CREATE TABLE calendar_date (
@@ -68,8 +70,16 @@ CREATE TABLE journey_disruption (
   planned_end TIMESTAMP,
   journey UUID
 );
+
+CREATE TABLE update_history (
+  id SERIAL PRIMARY KEY,
+  time TIMESTAMP,
+  status TEXT
+);
+
 ALTER TABLE stop ADD FOREIGN KEY(area) REFERENCES area;
 ALTER TABLE journey ADD FOREIGN KEY(route) REFERENCES route;
+ALTER TABLE journey ADD FOREIGN KEY (terminal_station) REFERENCES stop;
 ALTER TABLE calendar_date ADD FOREIGN KEY(journey) REFERENCES journey;
 ALTER TABLE calendar_exception ADD FOREIGN KEY(calendar_date) REFERENCES calendar_date;
 ALTER TABLE schedule ADD FOREIGN KEY(stop) REFERENCES stop;
@@ -79,6 +89,7 @@ ALTER TABLE journey_disruption ADD FOREIGN KEY(journey) REFERENCES journey;
 
 CREATE INDEX area_on_stop_fkey_index ON stop(area);
 CREATE INDEX route_on_journey_fkey_index ON journey(route);
+CREATE INDEX terminal_station_on_journey_fkey_index ON journey(terminal_station);
 CREATE INDEX journey_on_calendar_date_fkey_index ON calendar_date(journey);
 CREATE INDEX calendar_date_on_calendar_exception_fkey_index ON calendar_exception(calendar_date);
 CREATE INDEX stop_on_schedule_fkey_index ON schedule(stop);
