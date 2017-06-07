4d64e956209ad26256a9742d60a8689efc605839
matteopatisso <matteo.patisso@gmail.com>
2017-06-07 20:18:43 +0200

    Add ScheduleUpdate to shared project

---
+++ src/main/java/ch/bernmobil/vibe/shared/entitiy/ScheduleUpdate.java
@@ -0,0 +1,55 @@
+package ch.bernmobil.vibe.shared.entitiy;
+
+import java.sql.Time;
+import java.util.UUID;
+
+public class ScheduleUpdate {
+
+    private UUID id;
+    private Time actualArrival;
+    private Time actualDeparture;
+    private UUID schedule;
+
+    public ScheduleUpdate(Time actualArrival, Time actualDeparture, UUID schedule) {
+        this.actualArrival = actualArrival;
+        this.actualDeparture = actualDeparture;
+        this.schedule = schedule;
+    }
+
+    public ScheduleUpdate(UUID id, Time actualArrival, Time actualDeparture, UUID schedule) {
+        this(actualArrival, actualDeparture, schedule);
+        this.id = id;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public Time getActualArrival() {
+        return actualArrival;
+    }
+
+    public void setActualArrival(Time actualArrival) {
+        this.actualArrival = actualArrival;
+    }
+
+    public Time getActualDeparture() {
+        return actualDeparture;
+    }
+
+    public void setActualDeparture(Time actualDeparture) {
+        this.actualDeparture = actualDeparture;
+    }
+
+    public UUID getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(UUID schedule) {
+        this.schedule = schedule;
+    }
+}
